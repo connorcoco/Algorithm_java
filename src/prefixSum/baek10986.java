f1892a1372b4657d3f42cca74091b5006e09f538
JavaNo0b <98101954+JavaNo0b@users.noreply.github.com>
2025-01-06 15:36:44 +0900

    day2(1546,11720,10986,11660)

--- src/prefixSum/baek10986.java
+++ src/prefixSum/baek10986.java
@@ -9,18 +9,24 @@ public class baek10986 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int M = sc.nextInt();
-        int[] S = new int[N+1];
+        long[] S = new long[N+1];
+        long[] arr = new long[M];
 
         for (int n = 1; n < N+1; n++){
-            int a = sc.nextInt();
+            long a = sc.nextInt();
             S[n] = S[n-1] + a;
         }
-        int sum = 0;
-        for (int j = 1; j < N+1; j++){
-            for (int i = 1; i < j+1; i++){
-                if((S[j] - S[i-1]) % M == 0){
-                    sum++;
-                }
+
+        for (int n = 1; n < N+1; n++){
+            int ramainNum = (int) (S[n] % M);
+            arr[ramainNum]++;
+        }
+
+        long sum = 0;
+        for (int i = 0; i < M; i++){
+            if(i == 0) sum += arr[0];
+            if(arr[i] > 1){
+                sum += arr[i]*(arr[i]-1)/2;
             }
         }
         System.out.println(sum);
