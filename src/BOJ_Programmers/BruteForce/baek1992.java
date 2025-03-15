dc3112663aa0574117168e45b96397807610efc0
JavaNo0b <98101954+JavaNo0b@users.noreply.github.com>
2025-03-15 16:01:48 +0900

    week2 : [BOJ / Brute Force] 1992 진행 중

---
+++ src/BOJ_Programmers/BruteForce/baek1992.java
@@ -0,0 +1,21 @@
+// week2 : [BOJ / Brute Force] 1992 (쿼드 트리 / 실버 1)
+
+package BOJ_Programmers.BruteForce;
+
+import java.io.*;
+import java.util.*;
+
+public class baek1992 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+
+            for (int j = 0; j < N; j++) {
+                st.nextToken();
+            }
+        }
+    }
+}
