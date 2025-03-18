99d5dee05a67567c6abb7b5b581fd6672550d3a6
JavaNo0b <98101954+JavaNo0b@users.noreply.github.com>
2025-03-19 00:42:03 +0900

    week2 : [BOJ / Tree] 1991

--- src/BOJ_Programmers/Tree/baek1991.java
+++ src/BOJ_Programmers/Tree/baek1991.java
@@ -6,6 +6,8 @@ import java.io.*;
 import java.util.*;
 
 public class baek1991 {
+    static StringBuilder sb;
+
     public static void main(String[] args) throws Exception{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bf.readLine());
@@ -15,12 +17,18 @@ public class baek1991 {
         Tree binaryTree = new Tree();
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(bf.readLine());
-            for (int j = 0; j < 3; j++) {
-                binaryTree.insert(st.nextToken());
-            }
+            String parentValue = st.nextToken();
+            String leftValue = st.nextToken();
+            String rightValue = st.nextToken();
+            binaryTree.insert(parentValue, leftValue, rightValue);
         }
-
-
+        sb = new StringBuilder();
+        binaryTree.before(sb, binaryTree.root);
+        sb.append("\n");
+        binaryTree.middle(sb, binaryTree.root);
+        sb.append("\n");
+        binaryTree.after(sb, binaryTree.root);
+        System.out.println(sb);
     }
 
     static class Node {
@@ -33,22 +41,6 @@ public class baek1991 {
             this.left = null;
             this.right = null;
         }
-
-        Node getLeft() {
-            return left;
-        }
-
-        void setLeft(Node left) {
-            this.left = left;
-        }
-
-        Node getRight() {
-            return right;
-        }
-
-        void setRight(Node right) {
-            this.right = right;
-        }
     }
 
     static class Tree {
@@ -58,12 +50,43 @@ public class baek1991 {
             this.root = null;
         }
 
-        void insert(String value) {
+        void insert(String pValue, String lValue, String rValue) {
             if (root == null) {
-                root = new Node(value);
-            } else {
-
+                root = new Node(pValue);
             }
+            insertNode(root, pValue, lValue, rValue);
+        }
+
+        void insertNode(Node parent, String pValue, String lValue, String rValue) {
+            if (parent == null) return;
+
+            if (parent.value.equals(pValue)) {
+                if (!lValue.equals(".")) parent.left = new Node(lValue);
+                if (!rValue.equals(".")) parent.right = new Node(rValue);
+            } else {
+                // root부터 가지로 한층씩 탐색 -> pValue에 해당하는 Node 찾기 위함
+                insertNode(parent.left, pValue, lValue, rValue);
+                insertNode(parent.right, pValue, lValue, rValue);
+            }
+        }
+
+        void before(StringBuilder sb, Node node) {
+            if (node == null) return;
+            sb.append(node.value);
+            before(sb, node.left);
+            before(sb, node.right);
+        }
+        void middle(StringBuilder sb, Node node) {
+            if (node == null) return;
+            middle(sb, node.left);
+            sb.append(node.value);
+            middle(sb, node.right);
+        }
+        void after(StringBuilder sb, Node node) {
+            if (node == null) return;
+            after(sb, node.left);
+            after(sb, node.right);
+            sb.append(node.value);
         }
     }
 }
