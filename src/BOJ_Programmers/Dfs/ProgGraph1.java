29a4d8865d34dd68c9de508fc3c7c3de81c246c0
JavaNo0b <98101954+JavaNo0b@users.noreply.github.com>
2025-03-26 17:35:07 +0900

    // week3 : [Programmers / Set] 폰켓몬

---
+++ src/BOJ_Programmers/Dfs/ProgGraph1.java
@@ -0,0 +1,6 @@
+// week3 : [Programmers / Graph] 게임 맵 최단거리 (lv2)
+
+package BOJ_Programmers.Dfs;
+
+public class ProgGraph1 {
+}
---
+++ src/BOJ_Programmers/Set/progSet1.java
@@ -0,0 +1,16 @@
+// week3 : [Programmers / Set] 폰켓몬 (lv1)
+
+package BOJ_Programmers.Set;
+
+import java.util.*;
+
+public class progSet1 {
+    public int solution(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+        for (int num: nums) {
+            set.add(num);
+        }
+        return Math.max(set.size(), nums.length/2);
+    }
+}
