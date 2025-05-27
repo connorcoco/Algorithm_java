b57790f0ca6cd3ae88c7c8b5a3255afac17f4dd6
JavaNo0b <98101954+JavaNo0b@users.noreply.github.com>
2025-05-27 20:12:47 +0900

    dfs 복습 백준 1987

---
+++ src/BOJ_Programmers/Dfs/Baek1987_2.java
@@ -0,0 +1,54 @@
+package BOJ_Programmers.Dfs;
+
+import java.io.*;
+import java.util.*;
+
+public class Baek1987_2 {
+    static int R, C;
+    static int max = 0;
+    static char[][] graph;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        graph = new char[R][C];
+        Set<Character> visited = new HashSet<>();
+
+        // 그래프 작성
+        for (int i = 0; i < R; i++) {
+            String str = bf.readLine();
+            for (int j = 0; j < C; j++) {
+                graph[i][j] = str.charAt(j);
+            }
+        }
+
+        dfs(0, 0, 0, visited);
+        System.out.println(max);
+    }
+
+    public static void dfs(int y, int x, int count, Set<Character> visited) {
+        // 길이 아닌 곳 또는 같은 알파벳 발견
+        if (y >= R || y < 0 || x >= C || x < 0) {
+            if (max < count) {
+                max = count;
+            }
+            return;
+        } else if (visited.contains(graph[y][x])) {
+            if (max < count) {
+                max = count;
+            }
+            return;
+        }
+
+        visited.add(graph[y][x]);
+        count++;
+        dfs(y-1, x, count, visited);
+        dfs(y+1, x, count, visited);
+        dfs(y, x+1, count, visited);
+        dfs(y, x-1, count, visited);
+        visited.remove(graph[y][x]);
+    }
+}
